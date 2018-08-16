f5fb451fbbd5bb91fa0c4370ee4647cb0d335e18
DESKTOP-U06PV7I\AmithS <amith.shivantha@conxillium.com>
2018-08-16 09:35:57 +0530

    ngClass directive

--- project/ng-fundamentals/src/app/events/event-item.component.ts
+++ project/ng-fundamentals/src/app/events/event-item.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
     templateUrl: './event-item.component.html',
     styles: [
         `
+        .bold {font-weight:bold;}
+        .green {color:#003300;}
         .some-bold {font-weight:bold;}
         `
     ]
@@ -25,4 +27,16 @@ export class EventItemComponent implements OnInit {
         console.log('foo from parent');
     }
     ngOnInit(): void { }
+
+    getStartTimeCSS() {
+        const isEarlyStart = this.event && this.event.time === '8:00 am';
+        return {green: isEarlyStart, bold: isEarlyStart };
+
+        // same as
+        // if (this.event && this.event.time === '8:00 am') {
+        //     return 'green bold';
+        // } else {
+        //     return '';
+        // }
+    }
 }
