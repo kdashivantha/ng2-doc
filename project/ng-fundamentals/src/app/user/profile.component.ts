432c1b2f918accf179c1f850f0de2c4685b53d8f
DESKTOP-U06PV7I\AmithS <amith.shivantha@conxillium.com>
2018-08-17 15:22:43 +0530

    form validators in reactive forms

--- project/ng-fundamentals/src/app/user/profile.component.ts
+++ project/ng-fundamentals/src/app/user/profile.component.ts
@@ -5,33 +5,50 @@ import { Router } from '@angular/router';
 
 @Component({
     // selector: 'app-name',
-    templateUrl: './profile.component.html'
+    templateUrl: './profile.component.html',
+    styles: [`
+        em { float:right; color:#E05C65;padding-left: 10px;}
+        .error input { background-color:#E3C3C5;}
+        .error ::-webkit-input-placeholder { color: #999; }
+        .error ::-moz-placeholder { color:#999; }
+        .error :-moz-placeholder { color:#999; }
+        .error :ms-input-placeholder { color:#999; }
+    `]
     // styleUrls: ['./name.component.scss']
 })
 export class ProfileComponent implements OnInit {
     profileForm: FormGroup;
+    private firstName: FormControl;
+    private lastName: FormControl;
 
     constructor(private auth: AuthService, private router: Router) { }
 
     ngOnInit(): void {
-        let firstName = new FormControl(
-            this.auth.currentUser.firstName, Validators.required
+        this.firstName = new FormControl(
+            this.auth.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')]
         );
-        let lastName = new FormControl(
+        this.lastName = new FormControl(
             this.auth.currentUser.lastName, Validators.required
         );
 
         this.profileForm = new FormGroup({
-            firstName: firstName,
-            lastName: lastName
+            firstName: this.firstName,
+            lastName: this.lastName
         });
-     }
+    }
 
-    cancel() {
-        this.router.navigate(['events']);
+    validateLastName() {
+        return this.lastName.valid || this.lastName.touched;
+    }
+    validateFirstName() {
+        return this.firstName.valid || this.firstName.touched;
     }
     saveProfile(formValues) {
         this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
+        this.router.navigate(['events']);
+    }
+
+    cancel() {
         this.router.navigate(['events']);
     }
 }
