72e944195448b097627e686fa1da9c241fc7e3fe
DESKTOP-U06PV7I\AmithS <amith.shivantha@conxillium.com>
2018-08-17 15:59:18 +0530

    complex form models with child objects map to form

--- project/ng-fundamentals/src/app/events/event-create.component.ts
+++ project/ng-fundamentals/src/app/events/event-create.component.ts
@@ -1,32 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { EventService } from './shared/events.service';
 
 @Component({
-    selector: 'app-event-create',
-    template: `
-    <h1>New Event </h1>
-    <hr>
-    <div class="col-md-6">
-        <h3>Create Event Form</h3>
-        <br>
-        <br>
-        <button type="submit" class="btn btn-primary">
-            Save
-        </button>
-        <button type="button" class="btn btn-default" (click)="clickCancel()">
-            Cancel
-        </button>
-    </div>
-    `
+    templateUrl: './event-create.component.html',
+    styles: [`
+        em { float:right; color:#E05C65;padding-left: 10px;}
+        .error input { background-color:#E3C3C5;}
+        .error ::-webkit-input-placeholder { color: #999; }
+        .error ::-moz-placeholder { color:#999; }
+        .error :-moz-placeholder { color:#999; }
+        .error :ms-input-placeholder { color:#999; }
+    `]
 })
 export class EventCreateComponent implements OnInit {
+    newEvent;
     // tslint:disable-next-line:no-inferrable-types
     isDirty: boolean = true;
-    constructor(private route: Router) { }
+    constructor(private route: Router, private eventService: EventService) { }
 
     ngOnInit(): void { }
 
-    clickCancel() {
+    cancel() {
+        this.route.navigate(['/events']);
+    }
+
+    saveEvent(formValues) {
+        this.eventService.saveEvent(formValues);
+        this.isDirty = false;
         this.route.navigate(['/events']);
     }
 }
