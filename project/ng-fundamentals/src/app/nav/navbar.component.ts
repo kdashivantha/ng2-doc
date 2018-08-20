e86b72cd17bbd6dcd96e4f7cd58738141fb19b7b
DESKTOP-U06PV7I\AmithS <amith.shivantha@conxillium.com>
2018-08-20 10:34:02 +0530

    import Jquery as a provider

--- project/ng-fundamentals/src/app/nav/navbar.component.ts
+++ project/ng-fundamentals/src/app/nav/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../user/auth.service';
+import { ISession } from '../shared/event.model';
+import { EventService } from '../events/shared/events.service';
 
 @Component({
     selector: 'app-navbar',
@@ -12,7 +14,17 @@ import { AuthService } from '../user/auth.service';
     `]
 })
 export class NavBarComponent implements OnInit {
-    constructor(public auth: AuthService) { }
+    searchTerm: string = '';
+    foundSessions: ISession[];
+    constructor(public auth: AuthService, private eventService: EventService) { }
 
     ngOnInit(): void { }
+
+    searchSessions (searchTerm) {
+        this.eventService.searchSessions(searchTerm).subscribe
+        (sessions => {
+            this.foundSessions = sessions;
+            console.log(this.foundSessions);
+        });
+    }
 }
